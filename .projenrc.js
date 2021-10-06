65dc351ebff0103975d5a8dbfdde4f4e0ced0597
Klemens Mang <klemens.mang@capmo.de>
2021-10-06 23:59:31 +0200

    Utilize projen

---
+++ .projenrc.js
@@ -0,0 +1,33 @@
+const {
+  AwsCdkConstructLibrary,
+  NodePackageManager,
+  ProjectType,
+} = require('projen');
+const project = new AwsCdkConstructLibrary({
+  author: 'Sebastian Schlecht',
+  authorAddress: 'sebastian.schlecht@capmo.de',
+  cdkVersion: '1.126.0',
+  defaultReleaseBranch: 'main',
+  name: '@capmo/cdk-lib',
+  packageManager: NodePackageManager.NPM,
+  npmRegistryUrl: 'https://npm.pkg.github.com',
+  npmTokenSecret: 'GITHUB_TOKEN',
+  repositoryUrl: 'git@github.com:capmo/cdk-lib.git',
+
+  cdkDependencies: [
+    '@aws-cdk/core',
+    '@aws-cdk/aws-iam',
+    '@aws-cdk/aws-s3',
+  ] /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */,
+  cdkTestDependencies: [
+    '@aws-cdk/assert',
+  ] /* AWS CDK modules required for testing. */,
+  // deps: [] /* Runtime dependencies of this module. */,
+  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],                       /* Build dependencies for this module. */
+  // packageName: undefined,            /* The "name" in package.json. */
+  projectType:
+    ProjectType.AwsCdkConstructLibrary /* Which type of project this is (library/app). */,
+  // release: undefined,                /* Add release management to this project. */
+});
+project.synth();
