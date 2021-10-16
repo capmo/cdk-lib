bc92bc1b43e89d34db5749d8c5b360816f2a6803
Klemens Mang <klemens.mang@capmo.de>
2021-10-16 23:26:51 +0200

    Removed legacy test

--- test/s3/secure-bucket.test.ts
+++ test/s3/secure-bucket.test.ts
@@ -2,13 +2,6 @@ import { Stack } from '@aws-cdk/core';
 import { SecureBucket } from '../../src/s3';
 
 describe('SecureBucket', () => {
-  it('to have a security policy attached', () => {
-    const stack = new Stack();
-    const bucket = new SecureBucket(stack, 'secure-bucket');
-
-    expect(bucket.policy?.document).toBeDefined();
-  });
-
   it('to use server side encryption', () => {
     const stack = new Stack();
     const bucket = new SecureBucket(stack, 'secure-bucket');
