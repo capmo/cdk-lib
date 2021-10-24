07277d526b5c6542487b00db17896e6f3c68da76
Klemens Mang <klemens.mang@capmo.de>
2021-10-24 18:02:08 +0200

    Extended SecureBucket test suite

--- test/s3/secure-bucket.test.ts
+++ test/s3/secure-bucket.test.ts
@@ -1,7 +1,8 @@
 import { Key } from '@aws-cdk/aws-kms';
+import { BucketEncryption, Bucket, BlockPublicAccess } from '@aws-cdk/aws-s3';
 import { Stack } from '@aws-cdk/core';
 import * as Chance from 'chance';
-import { SecureBucket } from '../../src/s3';
+import { S3InventoryEnforceSSETagKey, SecureBucket } from '../../src/s3';
 
 jest.mock('@aws-cdk/aws-kms', () => ({
   Key: {
@@ -9,17 +10,70 @@ jest.mock('@aws-cdk/aws-kms', () => ({
   },
 }));
 
+jest.mock('@aws-cdk/aws-s3', () => ({
+  ...jest.requireActual('@aws-cdk/aws-s3'),
+  Bucket: jest.fn(),
+}));
+
+const tagAddMock = jest.fn();
+jest.mock('@aws-cdk/core', () => ({
+  ...jest.requireActual('@aws-cdk/core'),
+  Tags: {
+    of: jest.fn(() => ({
+      add: tagAddMock,
+    })),
+  },
+}));
+
 const chance = new Chance();
 
 describe('SecureBucket', () => {
-  it('to use server side encryption', () => {
-    (Key.fromLookup as jest.Mock).mockReturnValueOnce({
+  const bucketMock = (Bucket as unknown as jest.Mock);
+
+  beforeEach(() => {
+    bucketMock.mockClear();
+    tagAddMock.mockClear();
+  });
+
+  it('to use SSE', () => {
+    const encryptionKey = {
       keyId: chance.guid(),
-    });
+    };
+
+    (Key.fromLookup as jest.Mock).mockReturnValueOnce(encryptionKey);
 
     const stack = new Stack();
-    const bucket = new SecureBucket(stack, 'secure-bucket');
+    new SecureBucket(stack, 'secure-bucket');
 
-    expect(bucket.encryptionKey).toBeDefined();
+    expect(bucketMock.mock.calls[0][2]).toMatchObject({
+      bucketKeyEnabled: true,
+      encryption: BucketEncryption.KMS,
+      encryptionKey,
+    });
+  });
+
+  it('to block public access', () => {
+    const stack = new Stack();
+    new SecureBucket(stack, 'secure-bucket');
+
+    expect(bucketMock.mock.calls[0][2]).toMatchObject({
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    });
+  });
+
+  it('to enforce SSL in transit', () => {
+    const stack = new Stack();
+    new SecureBucket(stack, 'secure-bucket');
+
+    expect(bucketMock.mock.calls[0][2]).toMatchObject({
+      enforceSSL: true,
+    });
+  });
+
+  it('to set S3 inventory enforce SSE tag', () => {
+    const stack = new Stack();
+    new SecureBucket(stack, 'secure-bucket');
+
+    expect(tagAddMock).toHaveBeenCalledWith(S3InventoryEnforceSSETagKey, 'true');
   });
 });
